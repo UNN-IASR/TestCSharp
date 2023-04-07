403f0a78a31ce2ea761c62c4b2c56a88bd9b6de4
anton <ashtanyuk@gmail.com>
2023-04-07 08:30:00 +0300

    2023, data files

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -5,6 +5,15 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        
+        [TestMethod()]
+        [DeploymentItem("testFile1.xml")]
+        public void ConstructorTest()
+        {
+            string file = "testFile1.xml";
+            Assert.IsTrue(File.Exists(file), "deployment failed: " + file +
+                " did not get deployed");
+        }
         [TestMethod]
         public void TestMethod1()
         {
@@ -16,6 +25,12 @@ namespace UnitTestProject1
         {
             int result = ConsoleApp1.Program.add(3, 4);
             Assert.IsTrue(result == 7);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            Assert.IsTrue(true);
         }
     }
 }
